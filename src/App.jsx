06d98fa42782a39bf352974f9a1ebe03dd69744f
GiuseppeColucci95 <colucci539@gmail.com>
2025-03-27 15:52:45 +0100

    a dd base exercise with useState and useEffect
    -Implement movie filtering functionality and update UI elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-const films = [
+import { useEffect, useState } from "react";
+
+const data = [
   { title: 'Inception', genre: 'Fantascienza' },
   { title: 'Il Padrino', genre: 'Thriller' },
   { title: 'Titanic', genre: 'Romantico' },
@@ -9,15 +11,29 @@ const films = [
 
 function App() {
 
+  //logic here
+  const [films, setFilms] = useState(data);
+  const [genre, setGenre] = useState('');
+  const [filteredFilms, setFilteredFilms] = useState(films);
+
+  useEffect(() => {
+
+    console.log("Changed genre!", genre);
+    setFilteredFilms(films.filter(film => film.genre.toLowerCase() === genre.toLowerCase() || genre === ''));
+
+  }, [genre]);
+
+
+  //return here
   return (
     <>
       <div className="container text-center py-3">
 
-        <h1>React Movie Filter</h1>
+        <h1>Choose a Movie!</h1>
 
         <section>
 
-          <select className="my-3" name="genre" id="genre">
+          <select onChange={(e) => setGenre(e.target.value)} className="my-3" name="genre" id="genre">
             <option value="">Select a genre</option>
             <option value="Thriller">Thriller</option>
             <option value="Fantascienza">Fantascienza</option>
@@ -26,10 +42,9 @@ function App() {
           </select>
 
           <div className="row d-flex row-gap-3">
-
             {
-              films.map(film => (
-                <div className="col-4">
+              filteredFilms.map(film => (
+                <div key={film.title} className="col-4">
                   <div className="card p-3">
                     <h3 className="m-0">{film.title}</h3>
                     <p className="m-0">{film.genre}</p>
@@ -37,9 +52,6 @@ function App() {
                 </div>
               ))
             }
-
-
-
           </div>
 
         </section>
